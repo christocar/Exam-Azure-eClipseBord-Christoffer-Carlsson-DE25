3ca47a71a4d70a3aa1d37b8a53ec1568aa9a2253
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2026-08-31 21:00:47 +0200

    Add Terraform config for app service plan and backend web app

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,34 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+# the server that the web apps run on, shared between backend and frontend
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-${var.project_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "backend" {
+  name                = "app-${var.project_name}-backend-${var.unique_suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.plan.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      docker_image_name   = "eclipsebord-backend:v1"
+      docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
+
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+    }
+  }
+
+  # Azure needs to know which port the container listens on
+  app_settings = {
+    WEBSITES_PORT = "8000"
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,7 @@ output "acr_login_server" {
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "backend_url" {
+  value = "https://${azurerm_linux_web_app.backend.default_hostname}"
+}
